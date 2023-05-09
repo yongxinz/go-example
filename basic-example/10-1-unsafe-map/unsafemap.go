8e12af7b1c8f5ea505a2d083f0f871fd0b65e8fd
yongxinz <hi_nature@yeah.net>
2023-05-09 11:19:33 +0800

    feat: add unsafe map example

---
+++ basic-example/10-1-unsafe-map/unsafemap.go
@@ -0,0 +1,27 @@
+package main
+
+import "sync"
+
+func main() {
+	m := make(map[string]int)
+	m["foo"] = 1
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			m["foo"]++
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			m["foo"]++
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
