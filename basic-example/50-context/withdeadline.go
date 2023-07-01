4fd0008aae8911d4a6a4574f7da2fcfd68e56b2a
yongxinz <hi_nature@yeah.net>
2023-07-01 18:53:39 +0800

    feat: add context

---
+++ basic-example/50-context/withdeadline.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+const shortDuration = 1 * time.Millisecond
+
+func main() {
+	d := time.Now().Add(shortDuration)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+
+	// Even though ctx will be expired, it is good practice to call its
+	// cancellation function in any case. Failure to do so may keep the
+	// context and its parent alive longer than necessary.
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
